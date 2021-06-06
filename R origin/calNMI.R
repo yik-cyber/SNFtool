c3758ab796a77b461daa28411a5a0852621ed1d1
yik-cyber <yik_cyber@yeah.net>
2021-06-06 12:04:14 +0800

    delete

--- R origin/calNMI.R
+++
@@ -1,9 +0,0 @@
-calNMI <- function(x, y) {
-##This function calculate the NMI between two clusters.	
-	
-	x = as.vector(x);
-	y = as.vector(y);  
-    return(max(0, .mutualInformation(x, y)/sqrt(.entropy(x) * .entropy(y)), na.rm=TRUE))
-}
-
-
